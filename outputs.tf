2ef1ac750b3e4b083b274e563cf94f710d1ba369
Justin Campbell <justin@justincampbell.me>
2017-09-19 10:08:10 -0400

    Add description to outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,4 @@
 output "website_endpoint" {
-  value = "${aws_s3_bucket.default.website_endpoint}"
+  description = "The DNS name to point CNAME records to."
+  value       = "${aws_s3_bucket.default.website_endpoint}"
 }
