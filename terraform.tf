59470fb1c501fa9a99ca93bda0e10bac6960280c
Justin Campbell <justin@justincampbell.me>
2017-09-19 09:36:49 -0400

    Remove DNSimple and host var

--- terraform.tf
+++ terraform.tf
@@ -2,13 +2,6 @@ provider "aws" {
   region = "${var.region}"
 }
 
-resource "dnsimple_record" "default" {
-  domain = "${var.domain}"
-  name   = "${var.host}"
-  type   = "CNAME"
-  value  = "${aws_s3_bucket.default.website_endpoint}"
-}
-
 resource "aws_s3_bucket" "default" {
   bucket = "${var.bucket}"
   acl    = "public-read"
--- vars.tf
+++ vars.tf
@@ -1,15 +1,9 @@
-variable "domain" {
-  description = "Your domain name."
-  default     = "terraform-aws-staticsite-test.com"
-}
-
-variable "host" {
-  default = "www"
-}
-
 variable "bucket" {
   description = "The name of the S3 bucket. This is recommended to be host+domain, such as www.justincampbell.me"
-  default     = "terraform-aws-staticsite-test"
+}
+
+variable "domain" {
+  description = "Your domain name."
 }
 
 variable "region" {
