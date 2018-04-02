aeb376e099f3a12444c143914509fb765db1155a
Kit Sunde <kitsunde@gmail.com>
2018-04-02 17:18:15 +0800

    Allow the index document to be configured

--- vars.tf
+++ vars.tf
@@ -15,3 +15,8 @@ variable "region" {
   description = "The AWS region."
   default     = "us-east-1"
 }
+
+variable "index_document" {
+  description = "Use a different index document"
+  default     = "index.html"
+}
