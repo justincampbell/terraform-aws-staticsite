43e6f9e3301a76c62914cf55ce8e8710d3ec351c
Kit Sunde <kitsunde@gmail.com>
2018-05-31 22:17:01 +0800

    Output the website domain (#4)

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "website_endpoint" {
   description = "The DNS name to point CNAME records to."
   value       = "${aws_s3_bucket.default.website_endpoint}"
 }
+
+output "website_domain" {
+  description = "The website_domain for the s3 bucket."
+  value       = "${aws_s3_bucket.default.website_domain}"
+}
